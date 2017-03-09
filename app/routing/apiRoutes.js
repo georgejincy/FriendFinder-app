c6f23871b5110ec69bc2c3097873d538df6c87df
georgejincy <jincydon101@gmail.com>
2017-03-09 12:05:33 -0500

    Add package.json

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,5 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on friendsdata.
+// ===============================================================================
